4c21e55df5a8265f0d0e1f1c5b0104ff76bb67cd
AbhiMehata <abhimehta1910@gmail.com>
2025-02-18 20:38:47 +0000

    this is variables committed

---
+++ 01-basic/02_variables.js
@@ -0,0 +1,26 @@
+ const  accountId = 12345678;
+ let  accountEmail = "abhi123@gmail.com";
+ var accountPassword = "123456";
+
+ accountCity = "Banswara"
+  let accountState;
+// accountId = 2 this line will be not print becouse of const. const variable is not changeable
+
+
+
+ /**accountPassword = 54321
+ accountEmail = "abhi321@gmail.com"
+
+ don't use let and var till must need in project and code becouse of these are changeable and it will be over wirte 
+ */
+
+  console.log(accountId);
+  console.log(accountEmail)
+  console.log(accountPassword);
+  console.log(accountCity)
+  console.log(accountState);
+
+//   first method to print anything console.log
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+// second method to print anything console.log with this breacat[]
+  
