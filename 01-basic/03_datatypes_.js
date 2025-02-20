9185c9cbbcca273765005e04148251751521418c
AbhiMehata <abhimehta1910@gmail.com>
2025-02-20 00:58:26 +0000

    primitives datatypes

---
+++ 01-basic/03_datatypes_.js
@@ -0,0 +1,32 @@
+"use strict" /** when u write use strict it is treat as a new js code verstoin */
+/**2types dataTypes
+  1 Primitives
+  *number 
+  *string 
+  *bigInt 
+  *simbo 
+  *Boolean 
+  *symbol 
+  *null 
+  *undefined **/
+  const number= 1234567890 /*return number */
+
+  const string = "abhishek" /*return string */
+
+  const bigInt = (4857555445581234556666) /*return bigInt */
+
+  const Boolean = true /*return boolean */
+  
+  const float = 3.5 /*return number */
+
+  // const undefined ; /* return undifined */
+
+// const null= null /* return object */
+
+  // consol.log(typeof veriable name) to chack type of datatypes 
+  
+
+  console.table([number,string,bigInt,Boolean,float])
+
+
+//   2 NonPrimitives
