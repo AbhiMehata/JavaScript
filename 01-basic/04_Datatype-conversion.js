702425bb17d53d6b34a9a135e4288cd04980b34b
AbhiMehata <abhimehta1910@gmail.com>
2025-02-20 22:18:02 +0000

    datatype conversion

---
+++ 01-basic/04_Datatype-conversion.js
@@ -0,0 +1,13 @@
+let Password = "1223" /*this is string not a Number*/
+let userName = "abhishek"
+let IsLogedIn = true
+
+console.log(Password);
+console.log(typeof Password);
+let userNumber = Number(Password) 
+/* always chack typeof */
+/*this a formula to change any datatype string to Number and number to boolean etc. first make a new veriable than put what u want in upercase*/
+console.log(typeof userNumber);
+
+console.log(userNumber);
+
