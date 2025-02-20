702f4fb44996f72d2477d6f47bf878065635bc8e
AbhiMehata <abhimehta1910@gmail.com>
2025-02-20 22:27:52 +0000

    some comant add in conversion file

--- 01-basic/04_Datatype-conversion.js
+++ 01-basic/04_Datatype-conversion.js
@@ -1,12 +1,20 @@
 let Password = "1223" /*this is string not a Number*/
 let userName = "abhishek"
 let IsLogedIn = true
+let userNumber = 5656
 
 console.log(Password);
 console.log(typeof Password);
-let userNumber = Number(Password) 
+let userNum = Number(Password) 
 /* always chack typeof */
 /*this a formula to change any datatype string to Number and number to boolean etc. first make a new veriable than put what u want in upercase*/
+/* all datatypes velu return  */ 
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
 console.log(typeof userNumber);
 
 console.log(userNumber);
