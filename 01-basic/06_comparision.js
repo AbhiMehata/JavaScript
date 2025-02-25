02590729442df5a410d072008596c4452ee9f030
AbhiMehata <abhimehta1910@gmail.com>
2025-02-25 19:59:58 +0000

    comparision

---
+++ 01-basic/06_comparision.js
@@ -0,0 +1,24 @@
+// console.log(3 < 2);    /*  this all are comparision false */
+// console.log(3 > 2);    /* true */
+// console.log( 3 <= 2);  /* false */
+// console.log( 3 >= 2);  /* true */
+// console.log( 3 == 2);  /* false */
+// console.log( 3 != 2);  /* true */
+// console.log( "3" > 2);  /* true */
+// console.log("3" < 2);  /* false */
+
+
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+
+console.log("2" === 2)
