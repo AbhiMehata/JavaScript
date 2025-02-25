1c5eed6c80172d5a042da57fde3797c438c91e72
AbhiMehata <abhimehta1910@gmail.com>
2025-02-25 02:56:12 +0000

    operation

---
+++ 01-basic/05_operation-conversion.js
@@ -0,0 +1,41 @@
+
+/********************************************************operation***************************************************************/ 
+const num = 33
+const nvalue = -num
+// console.log(num)
+// console.log(nvalue);
+// console.log(2+2);
+// console.log(2-1);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+const num1 = 100
+const num2 = 3
+const num3 = (num1 + num2) /* to join number num + num*/
+// console.log(num3);
+
+const str1 = "Abhi "
+const str2 = "Mehata"
+// // console.log(str1 + str2); to join string str + str
+
+// console.log("1" + 2)                   /* if we have just two number and one string and one just number it treat as string */ 
+
+// console.log(1 + "2");                  /* if we have just two number and one string and one just number it treat as string */
+
+// console.log("1" + 1 +2);               /* if we have three first one just string it treat as string */
+
+// console.log(1 + 3 + 3 + "2");          /* in this seen first 3 digit add as number than it we be string*/
+
+let gamecounter = 10 /* let is addable and changeable*/
+++gamecounter
+console.log(gamecounter);
+
+
+
+
+
+
+
+
