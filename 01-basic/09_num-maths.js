7d5f94cd7c1afb9dc1246339f9d02d67ea1c0175
AbhiMehata <abhimehta1910@gmail.com>
2025-03-05 02:56:37 +0000

    About num-maths

---
+++ 01-basic/09_num-maths.js
@@ -0,0 +1,76 @@
+const score = 200
+// console.log(score)
+
+
+const balance =  new Number (10);
+// console.log(balance);
+
+// console.log(balance.toString()); /* to get string value in number method */
+// console.log(typeof balance);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/* toFixed*/
+// console.log(balance.toFixed(2)); /* toFixed make decimal what we want like 100.00000   */
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/*toprecision*/
+
+const newNumber = 140.1647
+
+// console.log(newNumber.toPrecision(6));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/* toLOcaleString */
+
+const hundreds = 1000000000
+
+// console.log(hundreds.toLocaleString('en-IN')); /* toLOcaleString it is indian stander */
+// console.log( `this usa ${hundreds.toLocaleString()}`); /* toLOcaleString  it is usa stander*/
+
+// ++++++++++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); /* abs does neg to positive */
+
+// console.log(Math.round(7.2)); /* it gives round up value */ 
+
+// console.log(Math.ceil(6.2)); /* it gives always higher value */
+
+// console.log(Math.floor(6.6)); /* it gives always lower value */
+ 
+// console.log(Math.sqrt(25)); /* it does always squer value */
+
+// console.log(Math.min(1,5,6,8,)); /* to get min value */
+
+// console.log(Math.max(1,5,6,8,95)); /* to get max value */
+
+
+console.log(Math.random());
+
+console.log((Math.random()*10) + 1);
+
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
