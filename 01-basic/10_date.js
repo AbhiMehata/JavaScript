f37da798b82da2f0963f1b5c2559180a119ca48d
AbhiMehata <abhimehta1910@gmail.com>
2025-03-06 02:28:51 +0000

    about-date

---
+++ 01-basic/10_date.js
@@ -0,0 +1,69 @@
+/* ++++++++++++++++ Date ++++++++++++++++ */
+
+let myDate = new Date()
+
+
+
+// console.log(myDate.toString())
+
+// console.log(myDate.toDateString())
+
+// console.log(myDate.toISOString())
+
+// console.log(myDate.toJSON())
+
+// console.log(myDate.toLocaleDateString())
+
+// console.log(myDate.toTimeString())
+
+// console.log(myDate.toUTCString())
+
+// console.log(myDate.getTimezoneOffset())
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// to creat new date 
+
+
+// let myNewCreatDate = new Date(2025, 0, 19) /*to difine date*/
+
+// console.log(myNewCreatDate.toLocaleString());
+
+// let myNewtDateTime = new Date(2025, 0, 19, 7, 2) /* to difine date and time */
+
+// console.log(myNewtDateTime.toLocaleString());
+
+
+let bob = new Date("2025-02-25")
+// console.log(bob.toLocaleString());
+
+
+// Timestamp
+
+const myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(bob.getTime());
+// console.log(Date.now()/1000); /* to get second */ 
+// console.log(Math.floor (Date.now()/1000));
+
+
+const newdate = new Date(2025, 3, 5)
+console.log(newdate.getFullYear());
+console.log(newdate.getDate());
+console.log(newdate.getMonth());
+console.log(newdate.getTime());
+
+ newdate.toLocaleString("default", {
+    weekday: "long",
+    dateStyle: "full"
+ })
+
+
+
+
+
+
+
+
+
+
