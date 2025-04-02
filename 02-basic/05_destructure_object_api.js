3d259ffa53ca4c0dafc4241234b7f2c3e6126c44
AbhiMehata <abhimehta1910@gmail.com>
2025-04-02 23:49:17 +0000

    object-destructure and api

---
+++ 02-basic/05_destructure_object_api.js
@@ -0,0 +1,34 @@
+// object destructureing
+
+const course = {
+    courseName: "Web Development",
+    courseAouthorName: "Abhishek",
+    coursePrice: "$12",
+}
+
+// console.log(course.courseName)
+const  {courseAouthorName} = course; /* value extract */
+const  {courseAouthorName: aouthor} = course;
+console.log(courseAouthorName)
+console.log(aouthor)
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// LEARN HOW TO CONVERT API 
+// API ( to learn api use (randomuser me) and to api format use (json format) to use https://randomuser.me/api )
+
+// JSON is without const and  let and var empty curly bresies is json 
+ {
+name: "abhishek"
+lastName: "mehata"
+addres: "banswara"
+id: 1145
+}  
+
+
+// API can be in array Formate 
+[
+    {},
+    {},
+    {}
+]
