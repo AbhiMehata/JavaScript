5f661229d95da04d8bc3a3fb14b1b1e4240aec9a
AbhiMehata <abhimehta1910@gmail.com>
2025-04-15 20:17:53 +0000

    learn more about scope

--- 03-basic/03_scope.js
+++ 03-basic/03_scope.js
@@ -15,7 +15,44 @@ if (true) {
     let a = 100
     const b = 200
     var c = 300
-    console.log(a);
+    // console.log(a);
 }
+// +++++++++++++++++++++++++     Nestet Scope  +++++++++++++++++++++++++++
+
+function one() {
+    const userName = "abhishek"
+
+    function two() {
+        const website = "youtube"
+        // console.log(userName);
+        
+    }
+    // console.log(website);
+    two()
+}
+one()
 
 
+//+++++++++++++++++++++ Also we can do in if else +++++++++++++++++++++++
+if (true) {
+    const username = "abhishek"
+    if (username === "abhishek") {
+        const website = " youtube"
+    //  console.log(username + website);
+     
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+//++++++++++++++++++++++++++++ one more prectice and two type of function ++++++++++++++++++++++++++++    
+function addOne(num1) {
+    return num1 + 2
+}
+console.log(addOne(10))
+// this typr call will give error +++++++++++++++++++++++++++++++++++++++++++++++++++ 
+addTwo()
+const addTwo = function (num) {
+    return num + 5
+}
